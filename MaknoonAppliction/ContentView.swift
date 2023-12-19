b3a20d9f1477331cab6eb4238937406f53eced5e
maryamalsaggfa <maryam1xsa@gmail.com>
2023-12-19 08:29:18 -0800

    Initial Commit

---
+++ MaknoonAppliction/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MaknoonAppliction
+//
+//  Created by maryam on 06/06/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MaknoonAppliction/MaknoonApplictionApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaknoonApplictionApp.swift
+//  MaknoonAppliction
+//
+//  Created by maryam on 06/06/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct MaknoonApplictionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
