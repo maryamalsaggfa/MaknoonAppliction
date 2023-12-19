2711d7081eeb5676928a7ed11e034ad41a00cdc1
maryamalsaggfa <maryam1xsa@gmail.com>
2023-12-19 10:46:19 -0800

    maryam-Happy state Pgaes 1

---
+++ MaknoonAppliction/feelingHappyState.swift
@@ -0,0 +1,18 @@
+//
+//  feelingHappyState.swift
+//  MaknoonAppliction
+//
+//  Created by maryam on 06/06/1445 AH.
+//
+
+import SwiftUI
+
+struct feelingHappyState: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    feelingHappyState()
+}
---
+++ MaknoonAppliction/humanHappyState.swift
@@ -0,0 +1,18 @@
+//
+//  humanHappyState.swift
+//  MaknoonAppliction
+//
+//  Created by maryam on 06/06/1445 AH.
+//
+
+import SwiftUI
+
+struct humanHappyState: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    humanHappyState()
+}
